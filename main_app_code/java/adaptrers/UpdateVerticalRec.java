255f9c91873e89209f7f0a0588234c0afca06e2d
SarangSudheer <163667793+SarangSudheer@users.noreply.github.com>
2024-11-09 19:26:08 +0530

    Add files via upload

---
+++ main_app_code/java/adaptrers/UpdateVerticalRec.java
@@ -0,0 +1,10 @@
+package com.example.food_ordering_app.adapters;
+
+import com.example.food_ordering_app.models.HomeVerModel;
+
+import java.util.ArrayList;
+
+public interface UpdateVerticalRec {
+    public void callBack(int position, ArrayList<HomeVerModel> List);
+
+}
